fb245b00f08be3f7e2497dcab65fcce4a594b997
ericktek <erickbale360@gmail.com>
2024-07-26 05:28:35 +0300

    dockerized bd

--- school/settings.py
+++ school/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,12 +79,18 @@ AUTH_USER_MODEL = 'api.User'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'school_db'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'erick123'),
+        'HOST': os.getenv('DB_HOST', 'mysql_db'),
+        'PORT': os.getenv('DB_PORT', 3306),
     }
 }
+
 
 
 # Password validation
